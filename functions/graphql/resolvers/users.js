df301f8c5728eb38ad6f5fc12e87dd4d14a89cb9
ghifari-ramadhan <71369040+ghifari-ramadhan@users.noreply.github.com>
2020-12-11 16:30:10 +0700

    Validate, user unique, to db DONE

---
+++ functions/graphql/resolvers/users.js
@@ -0,0 +1,76 @@
+const config = require('../../utility/config')
+const { UserInputError } = require('apollo-server-express')
+const encrypt = require('bcrypt')
+
+const firebase = require('firebase')
+const { db } = require('../../utility/admin')
+firebase.initializeApp(config)
+
+const { validateRegisterInput, validateLoginInput } = require('../../utility/validators')
+
+
+module.exports = {
+    Mutation: {
+        async register(_, { registerInput: {username, email, password, confirmPassword}}, context, info) {
+            // TODO: Validate user input 
+            // TODO: Make sure user is unique DONE
+            // TODO: Add user details to database DONE
+
+            const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword)
+
+            if(!valid) throw new UserInputError('Errors', { errors })
+
+            try {
+                let userCredentials = {
+                    username,
+                    email,
+                    createdAt: new Date().toISOString()
+                };
+
+                password = await encrypt.hash(password, 12)
+
+                await db.doc(`/users/${username}`)
+                        .get()
+                        .then(doc => {
+                            if (doc.exists) {
+                                throw new UserInputError('Username tidak tersedia', {
+                                    errors: { username: 'Username tidak tersedia' }
+                                })
+                            } else {
+                                return firebase.auth().createUserWithEmailAndPassword(email, password)
+                            }
+                        })
+                        .then(data => {
+                            userCredentials.id = data.user.uid
+                            return data.user.getIdToken()
+                        })
+                        .then(idToken => {
+                            userCredentials.token = idToken
+
+                            const userData = {
+                                id: userCredentials.id,
+                                username,
+                                email,
+                                createdAt: new Date().toISOString(),
+                                profilePicture: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/no-img.png?alt=media`,
+                                _private: []
+                            }
+                            userData._private.push({
+                                hash: password, 
+                                lastUpdate: new Date().toISOString()
+                            })
+
+                            return db.doc(`/users/${username}`).set(userData);
+                        })
+
+                return userCredentials
+            }
+            catch(err){
+                console.log(err)
+                if(err.code === 'auth/email-already-in-use') throw new UserInputError('Email yang anda input sudah digunakan pengguna lain')
+                throw new Error(err)
+            }
+                
+        }
+    }
+}
